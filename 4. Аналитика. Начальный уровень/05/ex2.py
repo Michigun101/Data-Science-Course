be2b7c8d223402cd08ba4be1c3049ddc063ce181
Michigun101 <mzanochuev@mail.ru>
2025-10-29 22:57:30 +0300

    Начало блока аналитики

---
+++ 4. Аналитика. Начальный уровень/05/ex2.py
@@ -0,0 +1,11 @@
+print('Hi')
+
+my_fam_hei = [
+    ['Mom',168],
+    ['Dad',186],
+    ['Son',200]
+]
+total_heigth = my_fam_hei[0][1]
+total_heigth += my_fam_hei[1][1]
+total_heigth += my_fam_hei[2][1]
+print('Tot Hie', total_heigth)
